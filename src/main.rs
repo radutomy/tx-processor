8960640f81b83fecaf5601d845fa838bf4955510
Radu T <radu@rtom.dev>
2025-08-25 00:28:51 +0100

    arg parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::env;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        anyhow::bail!("Usage: {} transactions.csv", args[0]);
+    }
+
+    Ok(())
 }
