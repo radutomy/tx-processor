ed8b582f3d628c10b3918c8a7d877fb733c4fcce
Radu T <radu@rtom.dev>
2025-08-25 00:33:44 +0100

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use anyhow::Result;
-use std::env;
+use anyhow::{Context, Result};
+use std::{env, fs::File};
+
+mod transactions;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -7,6 +9,17 @@ fn main() -> Result<()> {
     if args.len() != 2 {
         anyhow::bail!("Usage: {} transactions.csv", args[0]);
     }
+
+    Ok(())
+}
+
+fn process_transactions(input_path: &str) -> Result<()> {
+    let file =
+        File::open(input_path).with_context(|| format!("Failed to open file: {}", input_path))?;
+
+    let mut reader = csv::ReaderBuilder::new()
+        .trim(csv::Trim::All)
+        .from_reader(file);
 
     Ok(())
 }
